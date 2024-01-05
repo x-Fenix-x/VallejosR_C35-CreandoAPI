b99450bdf12bb5ad8e453ab94c02ef30b16b1f16
x-fenix-x <vallejosr@protonmail.com>
2024-01-05 13:19:59 -0300

    feat: cors

--- src/app.js
+++ src/app.js
@@ -2,10 +2,13 @@ const express = require('express');
 const paginate = require('express-paginate');
 const app = express();
 const PORT = 3001;
+const cors = require('cors');
 
 //URL encode  - Para que nos pueda llegar la información desde el formulario al req.body
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(cors());
 
 app.use(paginate.middleware(8, 50));
 
